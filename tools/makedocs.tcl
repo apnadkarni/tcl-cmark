9f35b27b57528eabe69cadd4896fa4582a4725cc
Ashok P. Nadkarni <apnmbx-wits@yahoo.com>
2025-08-21 18:26:15 +0530

    Remove duplicate makedocs!

--- tools/makedocs.tcl
+++ tools/makedocs.tcl
@@ -1,6 +1,6 @@
 set ver [package require cmark]
 
-set docdir [file join [file dirname [file dirname [info script]]] doc]
+set docdir [file join [file dirname [file dirname [file normalize [info script]]]] doc]
 
 set fd [open [file join $docdir tclcmark.md]]
 set mdtext [read $fd]
@@ -9,16 +9,16 @@ close $fd
 set fd [open [file join $docdir tclcmark.html] w]
 fconfigure $fd -encoding utf-8
 puts $fd "<!DOCTYPE html>\n<html>\n<head>\n<title>Tcl cmark extension</title>\n</head>\n<body>"
-puts $fd [cmark::render -to html -gfm -safe -smart $mdtext]
+puts $fd [cmark::render -to html -gfm -smart $mdtext]
 puts $fd "</body></html>"
 close $fd
 
 set fd [open [file join $docdir tclcmark.n] w]
 fconfigure $fd -encoding utf-8
 puts $fd {'\"}
-puts $fd {`\" Copyright (c) 2017 Ashok P. Nadkarni}
+puts $fd {'\" Copyright (c) 2017-2025 Ashok P. Nadkarni}
 puts $fd {'\"}
 puts $fd {'\" See file LICENSE in source for licensing terms.}
-puts $fd ".TH tclcmark n tclcmark $ver \"Tcl interface to CommonMark\""
-puts $fd [cmark::render -to man -gfm -safe -smart $mdtext]
+puts $fd ".TH tclcmark n tclcmark $ver \"Tcl Github-flavored Markdown processor\""
+puts $fd [cmark::render -to man -gfm -smart $mdtext]
 close $fd
